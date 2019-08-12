cd04b8a405e81db485ecc7e936eb46fee43e0f61
Gazouly <m.algazouly96@gmail.com>
2019-08-12 16:41:51 +0200

    Sending Responses

--- 01.Understanding.The.Basics/app.js
+++ 01.Understanding.The.Basics/app.js
@@ -6,6 +6,14 @@ const server = http.createServer((req, res) => {
     console.log('Request URL: ', req.url);
     console.log('Request METHOD: ', req.method);
     console.log('Request Headers: ', req.headers);
+
+    // Sending Responses
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Node App</title></head>');
+    res.write('<body><h1>Hello From Node.js App</h1></body>')
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000)
