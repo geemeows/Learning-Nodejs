75bd65ee56fdeaa080995b201ea7b35e4905eb1f
Gazouly <m.algazouly96@gmail.com>
2019-08-15 14:50:44 +0200

    Spliting Routes Logic from App.js

--- 01.Understanding.The.Basics/app.js
+++ 01.Understanding.The.Basics/app.js
@@ -1,44 +1,7 @@
 // Creating a Node Server
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes.js');
 
-const server = http.createServer((req, res) => {
-    // Routing
-    const url = req.url
-    const method = req.method
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>My Form</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send Message</button></form></body>')
-        res.write('</html>');
-        return res.end();
-    }
-    else if (url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-            console.log(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1]
-            // Blocking and Non-blocking Code
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        })
-    }
-
-    // Sending Responses
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Node App</title></head>');
-    res.write('<body><h1>Hello From Node.js App</h1></body>')
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000)
