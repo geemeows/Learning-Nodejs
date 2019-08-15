f389c6ecc5a8f5a04d8607ff239e9e1de3e7c713
Gazouly <m.algazouly96@gmail.com>
2019-08-15 20:28:11 +0200

    Using middlewares

--- 02.Working.with.Express.js/app.js
+++ 02.Working.with.Express.js/app.js
@@ -4,6 +4,17 @@ const express = require('express');
 
 const app = express();
 
+// Using middlewares
+app.use((req, res, next) => {
+    console.log('in the middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('in another middleware!');
+    // ..... Handling response 
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
