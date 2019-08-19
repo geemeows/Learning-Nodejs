8b46630c51cdd2c3f6e6dad88d0c2d891da9e0be
Gazouly <m.algazouly96@gmail.com>
2019-08-19 18:17:12 +0200

    Using Express Router

--- 02.Working.with.Express.js/app.js
+++ 02.Working.with.Express.js/app.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+// importing routes
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /> <button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Home Page</h1>');
-});
 
 app.listen(port, () => console.log(`Listening to port: ${port}.....`));
