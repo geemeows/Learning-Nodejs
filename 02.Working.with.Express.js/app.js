d6997b7a42b30a9246e9a677dd7d2eacb177b70d
Gazouly <m.algazouly96@gmail.com>
2019-08-21 20:05:22 +0200

    404 redirect request

--- 02.Working.with.Express.js/app.js
+++ 02.Working.with.Express.js/app.js
@@ -13,5 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 | Page Not Found</h1>')
+});
+
 
 app.listen(port, () => console.log(`Listening to port: ${port}.....`));
