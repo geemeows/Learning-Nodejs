b39445b60091de40cf9fafb1d3fe20be6f455252
Gazouly <m.algazouly96@gmail.com>
2019-09-01 20:00:53 +0200

    connecting to db

--- 06.SQL.Introduction/app.js
+++ 06.SQL.Introduction/app.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
