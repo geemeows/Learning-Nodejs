8bc1ce642d3b76bd45441d2bed4bf7222c84079d
Gazouly <m.algazouly96@gmail.com>
2019-09-01 20:12:25 +0200

    Refactoring the project and using MySQL

--- 06.SQL.Introduction/models/product.js
+++ 06.SQL.Introduction/models/product.js
@@ -11,6 +11,10 @@ module.exports = class Product {
   }
 
   save() {
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', 
+      [this.title, this.price, this.imageUrl, this.description]
+    );
 
   }
 
@@ -23,6 +27,9 @@ module.exports = class Product {
   }
 
   static findById(id) {
-
+    return db.execute(
+      'SELECT * FROM products WHERE products.id = ?',
+      [id]
+    );
   }
 };
