71c1468b8a887299dfeccc3ec546cfd9d52814de
Gazouly <m.algazouly96@gmail.com>
2019-09-04 19:45:03 +0200

    fetching products from db

--- 07.Working.with.NoSQL.Using.MongoDB/models/product.js
+++ 07.Working.with.NoSQL.Using.MongoDB/models/product.js
@@ -1,27 +1,57 @@
-const Sequelize = require('sequelize');
+const getDB = require('../util/database').getDB;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product  {
+  constructor(title, imageUrl, price, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
   }
-});
+
+  save() {
+    const DB = getDB();
+    return DB.collection('products')
+      .insertOne(this)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.log(err));
+  }
+
+  static fetchAll() {
+    const DB = getDB();
+    return DB.collection('products')
+              .find()
+              .toArray()
+              .then(res => {
+                console.log(res);
+                return res;
+              })
+              .catch(err => console.log(err));
+
+  }
+}
+
+// const Product = sequelize.define('product', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   },
+//   title: Sequelize.STRING,
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false
+//   },
+//   imageUrl: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   }
+// });
 
 module.exports = Product;
--- 07.Working.with.NoSQL.Using.MongoDB/util/database.js
+++ 07.Working.with.NoSQL.Using.MongoDB/util/database.js
@@ -1,13 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://Gazouly:k422UI8lbdkjPl4v@cluster0-ml4p7.mongodb.net/test?retryWrites=true&w=majority')
+  MongoClient.connect('mongodb+srv://Gazouly:k422UI8lbdkjPl4v@cluster0-ml4p7.mongodb.net/shop?retryWrites=true&w=majority')
     .then(client => {
       console.log('CONNECTED!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 }
 
-module.exports = mongoConnect;
+const getDB = () => {
+  if (_db) 
+    return _db;
+  else
+    throw 'NOT CONNECTED TO DB!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
