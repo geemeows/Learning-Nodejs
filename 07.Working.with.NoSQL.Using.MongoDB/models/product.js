69c11b752cc960fa6bbc9fc9923b774a50b7f61c
Gazouly <m.algazouly96@gmail.com>
2019-09-04 20:51:54 +0200

    update product in db

--- 07.Working.with.NoSQL.Using.MongoDB/models/product.js
+++ 07.Working.with.NoSQL.Using.MongoDB/models/product.js
@@ -1,17 +1,31 @@
+const mongodb = require('mongodb');
 const getDB = require('../util/database').getDB;
 
 class Product  {
-  constructor(title, imageUrl, price, description) {
+  constructor(title, imageUrl, price, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id;
   }
 
   save() {
     const DB = getDB();
-    return DB.collection('products')
-      .insertOne(this)
+    let dbOp;
+
+    if (this._id) {
+      // Update Product
+      dbOp = DB
+            .collection('products')
+            .updateOne({ _id: new mongodb.ObjectID(this._id) }, { $set: this }); 
+    } else {
+      // Create New Product
+      dbOp = DB
+            .collection('products')
+            .insertOne(this);
+    }
+    return dbOp
       .then(res => {
         console.log(res)
       })
@@ -24,11 +38,23 @@ class Product  {
               .find()
               .toArray()
               .then(res => {
-                console.log(res);
                 return res;
               })
               .catch(err => console.log(err));
 
+  }
+  static findByID(id) {
+    const DB = getDB();
+    return DB.collection('products')
+              .find({
+                _id: new mongodb.ObjectID(id)
+              })
+              .next()
+              .then(product => {
+                console.log(product);
+                return product;
+              })
+              .catch(err => console.log(err));
   }
 }
 
