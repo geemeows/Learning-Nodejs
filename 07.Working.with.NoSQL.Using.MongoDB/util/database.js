e42adba31ba445f6126bfdec86cc7fa958326e50
Gazouly <m.algazouly96@gmail.com>
2019-09-04 14:31:37 +0200

    connecting to mongodb

--- 07.Working.with.NoSQL.Using.MongoDB/util/database.js
+++ 07.Working.with.NoSQL.Using.MongoDB/util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://Gazouly:k422UI8lbdkjPl4v@cluster0-ml4p7.mongodb.net/test?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('CONNECTED!');
+      callback(client);
+    })
+    .catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
